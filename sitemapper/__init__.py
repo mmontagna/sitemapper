4604a5748643e32fa1c45becc3b0e8464e836b82
Marco Montagna <marco@mightysignal.com>
2018-06-01 11:30:32 -0700

    Add sitemapper module.

---
+++ sitemapper/__init__.py
@@ -0,0 +1,75 @@
+import os
+from datetime import datetime
+import xml.etree.cElementTree as ET
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    j = "\n" + (level-1)*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for subelem in elem:
+            indent(subelem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = j
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = j
+    return elem        
+
+
+def create_sitemap_index(sitemap_root, sitemap_files, output_file):
+  now = datetime.utcnow().isoformat()
+  sitemapindex = ET.Element("sitemapindex", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+  for sitemap_file in sitemap_files:
+    sitemap_node = ET.SubElement(sitemapindex, "sitemap")
+    loc = ET.SubElement(sitemap_node, "loc").text = "{}/{}".format(sitemap_root.rstrip('/'), sitemap_file)
+    lastmod = ET.SubElement(sitemap_node, "lastmod").text = now
+  tree = ET.ElementTree(sitemapindex)
+  indent(sitemapindex, level=0)
+  print("Writing sitemapindex to {}".format(output_file))
+  tree.write(output_file, xml_declaration=True, encoding='utf-8', method="xml")
+
+
+def create_sitemap(urls, output_file, changefreq='weekly'):
+  now = datetime.utcnow().date().isoformat()
+  urlset = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+  for url in urls:
+    url_node = ET.SubElement(urlset, "url")
+    loc = ET.SubElement(url_node, "loc").text = url
+    changefreq = ET.SubElement(url_node, "changefreq").text = changefreq
+  tree = ET.ElementTree(urlset)
+  indent(urlset, level=0)
+  print("Writing sitemap to {}".format(output_file))
+  tree.write(output_file, xml_declaration=True, encoding='utf-8', method="xml")
+  return output_file
+
+def create_sitemaps(sitemap_root, urls, output_directory, max_urls_per_sitemap=45000, changefreq='weekly'):
+  """
+  sitemap_root: The path where sitemaps will be served from eg http://example.com/
+  urls: a list of urls.
+  output_directory: The directory into which we'll store sitemaps and index files, it must not exist.
+  max_urls_per_sitemap: The max number of files to store per sitemap file (typically should be less than 50k)
+  changefreq: Create sitemap entries with changefreq equal to this.
+  """
+  if os.path.exists(output_directory):
+    print('Output directory {} already exists exiting.'.format(output_directory))
+    exit(1)
+  else:
+    os.mkdir(output_directory)
+  i = 0
+  sitemap_files = []
+  for url_chunk in chunks(urls, max_urls_per_sitemap):
+    sitemap_file = os.path.join(output_directory, 'sitemap{}.xml'.format(i))
+    create_sitemap(url_chunk, sitemap_file, changefreq=changefreq)
+    sitemap_files.append(sitemap_file)
+    i += 1
+  sitemapindex = os.path.join(output_directory, 'sitemapindex.xml')
+  create_sitemap_index(sitemap_root, sitemap_files, sitemapindex)
---
+++ sitemapper/__main__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import argparse
+
+from sitemapper import create_sitemaps
+
+def main():
+
+  parser = argparse.ArgumentParser(description='Create sitemap xml files.')
+
+  parser.add_argument('--input-file', required=True, help='A newline delimited text file which contains URLS')
+  parser.add_argument('--output-directory', required=True,
+    help='The output directory into which sitemap and sitemap index files will be written')
+  parser.add_argument('--change-freq', default='weekly', type=str)
+  parser.add_argument('--max-urls-per-sitemap', default=45000, type=int)
+  parser.add_argument('--sitemap-root', required=True, type=str)
+
+  args = parser.parse_args()
+
+  create_sitemaps(
+    args.sitemap_root,
+    open(args.input_file).read().split("\n"),
+    args.output_directory,
+    max_urls_per_sitemap=args.max_urls_per_sitemap,
+    changefreq=args.change_freq
+    )
+
+
+if __name__ == "__main__":
+    main()
