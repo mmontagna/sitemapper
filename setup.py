9ab23b89bdeb372a598f58f6d772143311c72aab
Marco Montagna <marco@mightysignal.com>
2018-06-01 16:02:11 -0700

    Bump to v1.1.1
    Fixes datetime formatting issue.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'sitemapper',
   packages = ['sitemapper'],
-  version = '1.1.0',
+  version = '1.1.1',
   description = '''
   A tool for generating xml sitemaps from a list of urls.
   ''',
