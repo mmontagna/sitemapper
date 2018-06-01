1a39946481122efa076f437c551ddbe4705e76d5
Marco Montagna <marco@mightysignal.com>
2018-06-01 12:13:18 -0700

    Bump to v1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'sitemapper',
   packages = ['sitemapper'],
-  version = '1.0.0',
+  version = '1.1.0',
   description = '''
   A tool for generating xml sitemaps from a list of urls.
   ''',
